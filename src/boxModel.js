cee727b8fab8e49405203cd9e3e1ef1c7bee2f8a
a.masalov <delsol@bk.ru>
2018-01-30 21:00:45 +0300

    add eslint

--- src/boxModel.js
+++ src/boxModel.js
@@ -1,5 +1,21 @@
+var flexbox = [
+  'flex',
+  'flex-grow',
+  'flex-shrink',
+  'flex-basis',
+  'flex-flow',
+  'flex-wrap',
+  'flex-direction',
+  'justify-content',
+  'align-items',
+  'align-self',
+  'align-content',
+  'order',
+];
+
 var boxModel = [
   'display',
+  ...flexbox,
   'overflow',
   'box-sizing',
   'width',
